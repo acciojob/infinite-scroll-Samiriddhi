716719797c98b2bd245f816bd952166924faf2fa
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-08-29 13:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here!
-
+    OlList.addEventListener('scroll',()=>{
+    	let height=OlList.offsetHeight;
+    let offset=OlList.scrollTop;
+     
+    if(offset+height>=OlList.scrollHeight){
+        console.log('at bottom');
+     
+        for(let i=1;i<=2;i++){
+            let li=document.createElement('li');
+            li.innerText=`Item ${++totalList}`;
+            OlList.append(li);
+            }
+    }
+    })
